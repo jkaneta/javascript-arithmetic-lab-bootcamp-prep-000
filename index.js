39af98350a003562b5c9d9d527d23eb28f97eae2
J <flatironschool@2joyce.com>
2018-01-12 18:24:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function divide(a,b) {
   return (a / b);
 }
 
+function int(n) {
+  return (n += 1);
+}
+
+function dec(n) {
+  return (n -= 1);
+}
+
+function makeInt(n) {
+  return parseInt(n);
+}
+
+function make
